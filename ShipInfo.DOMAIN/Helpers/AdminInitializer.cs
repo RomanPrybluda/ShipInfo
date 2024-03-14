aaf68ab825dfdaf0721c4f852971fce87886c0fe
Roman Prybluda <romanodua@gmail.com>
2024-03-14 12:51:26 +0200

    add changes for last commit

---
+++ ShipInfo.DOMAIN/Helpers/AdminInitializer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using ShipInfo.DAL;
+
+namespace Domain.Helpers
+{
+    public class AdminInitializer
+    {
+
+        public static async Task InitializeRole(UserManager<AppUser> userManager, IConfiguration configuration)
+        {
+            string adminEmail = "admin@weather.com";
+            string adminPassword = "AdminWeather!23";
+            var admin = await userManager.FindByEmailAsync(adminEmail);
+
+            if (admin == null)
+            {
+                admin = new AppUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    FirstName = "Admin",
+                    LastName = "Admin",
+                };
+
+                var result = await userManager.CreateAsync(admin, adminPassword);
+
+                if (!result.Succeeded)
+                {
+                    throw new Exception();
+                }
+
+                result = await userManager.AddToRoleAsync(admin, Roles.ADMIN);
+
+                if (!result.Succeeded)
+                {
+                    throw new Exception();
+                }
+
+            }
+
+
+        }
+    }
+}
